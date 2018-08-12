18f82438adc8dee35ce31817247f6dad669544fb
Unknown <vatanaksoytezer@gmail.com>
2018-08-12 16:09:46 +0300

    Prepare merge to master

--- src/main/java/org/usfirst/frc7108/Robot/sensors/Gyro.java
+++ src/main/java/org/usfirst/frc7108/Robot/sensors/Gyro.java
@@ -8,7 +8,7 @@ public class Gyro {
 	private double angle = 0; // The accumulated degrees from the get rotations per second function
 	private double rotation = 0; // The degrees of the blue gyro
 	private double rotatedAmount = 0;
-    // private I2C gyro = new I2C(I2C.Port.kOnboard, 0x68);
+    private I2C gyro = new I2C(I2C.Port.kOnboard, 0x68);
 		
 	public double getAngle () {
 		return angle;
@@ -41,7 +41,7 @@ public class Gyro {
     }
 
     private void setupGyro() {
-        /*
+        
 		gyro.write(0x6B, 0x00); // Power Config
 		gyro.write(0x1A, 0x23); // Basic Config
 		gyro.write(0x1B, 0x00); // Gyro Config
@@ -51,7 +51,7 @@ public class Gyro {
 		gyro.write(0x6A, 0x04); // User Control (Reset FIFO)
 		gyro.write(0x6A, 0x40); // User Control (Turn FIFO back on);
         gyro.write(0x19, 0x04); // Set sample rate to 200Hz
-        */     
+        
     }
 
     private int uByteToInt (byte number) {
@@ -83,11 +83,11 @@ public class Gyro {
 
     public boolean updateGyroFromSensor() {
  		byte[] dataReady = new byte[1];
-		// gyro.read(0x3A, 1, dataReady);
+		gyro.read(0x3A, 1, dataReady);
 		
 		if ((dataReady[0] & 0x00000001) > 0) {
 			byte[] angleByte = new byte[2];
-			// gyro.read(0x47, 2, angleByte);
+			gyro.read(0x47, 2, angleByte);
 			int highOrder = (int) angleByte[0];
 			int lowOrder = uByteToInt(angleByte[1]);
             
